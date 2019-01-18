780aaa30783f012b2b70bea22b039753d04480d3
Conrad Chase Metzger <chasemetzger15@gmail.com>
2019-01-18 02:07:08 -0800

    Added most, if not all, components that are going to be used in the app

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Cart extends React.Component {
+	render() {
+		return (
+			<div>
+				<h3>Hello from Cart</h3>
+			</div>
+		);
+	}
+}
---
+++ src/components/Details.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Details extends React.Component {
+	render() {
+		return (
+			<div>
+				<h3>Hello from Details</h3>
+			</div>
+		);
+	}
+}
