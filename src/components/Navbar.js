c7b8ad2965b73b5985293dc487a1ad543a4ebe85
Conrad Chase Metzger <chasemetzger15@gmail.com>
2019-01-18 22:42:17 -0800

    Do some more styling to the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { Link } from 'react-router-dom';
 
 import logo from '../logo.svg';
 
+import { ButtonContainer } from './Button';
+
 export default class Navbar extends React.Component {
 	render(){
 		return (
-			<nav className="navbar navbar-expand-sm navbar-dark pd-sm-5 bg-primary">
+			<NavWrapper className="navbar navbar-expand-sm navbar-dark pd-sm-5">
 			{/*https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk */}
@@ -22,12 +25,23 @@ https://www.iconfinder.com/Makoto_msk */}
 					</li>
 				</ul>
 				<Link to="/cart" className="ml-auto">
-					<button>
-						<i className="fas fa-cart-plus" />
+					<ButtonContainer>
+						<span className="mr-2">
+							<i className="fas fa-cart-plus" />
+						</span>
 						My Cart
-					</button>
+					</ButtonContainer>
 				</Link>
-			</nav>
+			</NavWrapper>
 		);
 	}
 }
+
+const NavWrapper = styled.nav`
+	background: var(--mainBlue);
+	.nav-link{
+		color: var(--mainWhite) !important;
+		font-size: 1.3rem;
+		text-transform: capitalize !important;
+	}
+`;
