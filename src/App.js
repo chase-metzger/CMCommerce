bcad91022c164292549d798c6af9f80dd98d9090
Conrad Chase Metzger <chasemetzger15@gmail.com>
2019-01-18 17:08:50 -0800

    Add react-router(-dom) and implement basic routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+//import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component {
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+
+class App extends React.Component {
 	render() {
 		return (
-			<div className="container">
-				<div className="row">
-					<div className="col-6">column number one</div>
-					<div className="col-6">
-						<span>
-							<i className="fas fa-home"/>
-						</span>
-					</div>
-				</div>
-			</div>
+			<React.Fragment>
+				<Navbar></Navbar>
+				<Switch>
+					<Route exact path="/" component={ProductList}></Route>
+					<Route path="/details" component={Details}></Route>
+					<Route path="/cart" component={Cart}></Route>
+					<Route component={Default}></Route>
+				</Switch>
+			</React.Fragment>
 		);
 	}
 }
