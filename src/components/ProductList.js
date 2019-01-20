faf2bd7ef7ca325b00b196419169441cb9285499
Conrad Chase Metzger <chasemetzger15@gmail.com>
2019-01-20 12:05:04 -0800

    Pretty much finished with the ProductList component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,29 @@
 import React from 'react';
+
 import Product from './Product';
+import Title from './Title';
+
+import { ProductConsumer } from '../context';
 
 export default class ProductList extends React.Component {
 	render() {
 		return (
-		<div>
-			<Product />
-		</div>
+			<React.Fragment>
+				<div className="py-5">
+					<div className="container">
+						<Title name="our" title="products" />
+						<div className="row">
+						<ProductConsumer>
+							{(value) => {
+								return value.products.map(product => {
+									return <Product key={product.id.toString()} product={product} />
+								})
+							}}
+						</ProductConsumer>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
 		)
 	}
 }
