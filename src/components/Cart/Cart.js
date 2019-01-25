7cbf2b843849a05e95be19fcca9fbac6fae1762c
Conrad Chase Metzger <chasemetzger15@gmail.com>
2019-01-24 23:54:19 -0800

    Add the initial CartTotals component implementation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,6 +6,7 @@ import CartList from './CartList';
 import EmptyCart from './EmptyCart';
 
 import { ProductContext } from '../../context';
+import CartTotals from './CartTotals';
 
 export default function Cart() {
 	const context = useContext(ProductContext);
@@ -17,6 +18,7 @@ export default function Cart() {
 				<Title name="your" title="cart" />
 				<CartColumns />
 				<CartList cart={cart} getItem={context.getItem} />
+				<CartTotals cart={cart} />
 			</React.Fragment>
 		);
 	} else {
