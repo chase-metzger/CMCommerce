316004ba57b9d0355639e36191c5d799ab8c7816
Conrad Chase Metzger <chasemetzger15@gmail.com>
2019-01-24 18:02:07 -0800

    Add the CartColumns component and use it in the Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import Title from '../Title';
+import CartColumns from './CartColumns';
+
 export default class Cart extends React.Component {
 	render() {
 		return (
-			<div>
-				<h3>Hello from Cart</h3>
-			</div>
+			<section>
+				<Title name="your" title="cart" />
+				<CartColumns />
+			</section>
 		);
 	}
 }
